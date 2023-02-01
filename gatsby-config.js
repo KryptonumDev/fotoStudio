9d981bf1282aa131a272d506ca9b4affd68e057a
Kryptonum <kryptonumstudio@gmail.com>
2023-02-01 14:16:26 +0100

    added DatoCms plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env`,
+})
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
@@ -6,12 +9,22 @@ module.exports = {
     title: `Foto Studio`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/resources/images/"
+      },
+      __key: "images"
     },
-    __key: "images"
-  }]
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.API_DATOCMS,
+        disableLiveReload: false,
+      },
+    },
+  ]
 };
