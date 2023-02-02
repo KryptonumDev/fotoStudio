91ae66d8620ce9aea737c1a00947c8b74965d46b
Kryptonum <kryptonumstudio@gmail.com>
2023-02-02 12:43:17 +0100

    remove onRenderBody from gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Layout from "./src/components/sections/Layout"
 
-export const onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: "pl" })
-}
-
 export const wrapPageElement = ({ props, element }) => {
   return <Layout {...props}>{element}</Layout>
 }
