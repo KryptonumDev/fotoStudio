2d1dbf7222f5092c501c1f0c0ff82fca5679e861
Kryptonum <kryptonumstudio@gmail.com>
2023-02-06 13:52:24 +0100

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,90 @@
 import * as React from "react"
+import styled from "styled-components";
+import Button from '../components/atoms/Button';
 
 const NotFoundPage = () => {
   return (
-    <p>Not found</p>
+    <StyledNotFound className="NotFound">
+      <div className="NotFound-content">
+        <div className="max-width">
+          <h1>Strona której szukasz nie istnieje</h1>
+          <Button link="/">Główna</Button>
+        </div>
+      </div>
+      <div className="NotFound-marquee">
+        <div><span>404</span><span>404</span><span>404</span><span>404</span><span>404</span></div>
+        <div><span>404</span><span>404</span><span>404</span><span>404</span><span>404</span></div>
+        <div><span>404</span><span>404</span><span>404</span><span>404</span><span>404</span></div>
+      </div>
+    </StyledNotFound>
   )
 }
 
+const StyledNotFound = styled.section`
+  position: relative;
+  min-height: 100vh;
+  min-height: 100dvh;
+  overflow: hidden;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  .NotFound-content {
+    width: 100%;
+    text-align: center;
+    z-index: 1;
+    padding: ${100/10.8}vh 0;
+    position: relative;
+    &::before {
+      content: '';
+      width: 100%;
+      height: 200%;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      background: linear-gradient(rgba(0,0,0,0), #000);
+      z-index: -1;
+    }
+    h1 {
+      font-size: max(${64/19.2}vw, ${30/16}rem);
+      margin-bottom: ${50/10.8}vh;
+    }
+  }
+  .NotFound-marquee {
+    position: absolute;
+    left: -2%;
+    right: -2%;
+    top: 0;
+    height: 100%;
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: flex-start;
+    justify-content: space-between;
+    & > div {
+      white-space: nowrap;
+      writing-mode: vertical-rl;
+      animation: marquee 8s infinite linear;
+      font-size: min(${200/10.8}vh, ${100/16}rem);
+      line-height: 1;
+      font-family: "DM Serif Display", serif;
+      &:nth-child(2){
+        animation-direction: reverse;
+      }
+      > span:not(:last-child) {
+        margin-bottom: ${89/10.8}vh;
+      }
+    }
+
+  }
+  @keyframes marquee {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-21%);
+    }
+  }
+`
+
 export default NotFoundPage
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>Strony nie znaleziono - Foto Studio</title>
