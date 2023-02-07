416e2e3f78567028072a2a2890d05fc34e5288eb
Kryptonum <kryptonumstudio@gmail.com>
2023-02-07 14:08:42 +0100

    changed button decoration size on mobile

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -5,9 +5,9 @@ import styled from "styled-components";
 const Button = ({link, children}) => {
   return (
     <StyledLink to={link}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="41" height="40" fill="#fff"><path d="M41 34.39 35.354 40H0V5.61L5.646 0v34.39H41Z"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="41" height="40" viewBox="0 0 41 40" fill="#fff"><path d="M41 34.39 35.354 40H0V5.61L5.646 0v34.39H41Z"/></svg>
       <span>{children}</span>
-      <svg xmlns="http://www.w3.org/2000/svg" width="41" height="40" fill="#fff"><path d="M0 5.61 5.646 0H41v34.39L35.354 40V5.61H0Z"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="41" height="40" viewBox="0 0 41 40" fill="#fff"><path d="M0 5.61 5.646 0H41v34.39L35.354 40V5.61H0Z"/></svg>
     </StyledLink>
   );
 }
@@ -29,24 +29,48 @@ const StyledLink = styled(gatsbyLink)`
     position: absolute;
     transition: transform .3s;
     &:first-child {
-      bottom: -6px;
-      left: -6px;
+      bottom: -5px;
+      left: -5px;
     }
     &:last-child {
-      top: -6px;
-      right: -6px;
+      top: -5px;
+      right: -5px;
     }
   }
   &:hover {
     svg {
       &:first-child {
-        transform: translate(6px, -6px);
+        transform: translate(5px, -5px);
       }
       &:last-child {
-        transform: translate(-6px, 6px);
+        transform: translate(-5px, 5px);
       }
     }
   }
+  @media only screen and (max-width: 1199px){
+    svg {
+      width: 25px;
+      height: 25px;
+      &:first-child {
+        bottom: -4px;
+        left: -4px;
+      }
+      &:last-child {
+        top: -4px;
+        right: -4px;
+      }
+    }
+    &:hover {
+      svg {
+        &:first-child {
+          transform: translate(3px, -3px);
+        }
+        &:last-child {
+          transform: translate(-3px, 3px);
+        }
+      }
+    }
+  }   
 `
  
 export default Button;
