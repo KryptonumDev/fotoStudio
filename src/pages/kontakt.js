0e2816c6164422e26b79563df0d96277cf1debba
Kryptonum <kryptonumstudio@gmail.com>
2023-02-10 09:58:35 +0100

    added kontakt page

---
+++ src/pages/kontakt.js
@@ -0,0 +1,45 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import ContactHero from "../components/sections/ContactHero"
+import ContactDetails from "../components/sections/ContactDetails"
+import Footer from "../components/organisms/Footer"
+import PageIndicator from "../components/organisms/PageIndicator"
+
+const ContactPage = ({data}) => {
+  const {hero, details} = data.contact;
+  return (
+    <div className="scrollSnap">
+      <PageIndicator>
+        <ContactHero data={hero} />
+        <ContactDetails data={{details, contact: data.footer}} />
+        <Footer data={data.footer} />
+      </PageIndicator>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query {
+    contact: datoCmsContact {
+      hero {
+        gatsbyImageData
+        alt
+      }
+      details {
+        gatsbyImageData
+        alt
+      }
+    }
+    footer: datoCmsFooter {
+      email
+      address
+      addressMaps
+      phone1
+      phone2
+      instagram
+      facebook
+    }
+  }
+`
+ 
+export default ContactPage;
