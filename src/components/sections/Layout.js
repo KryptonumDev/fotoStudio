44a52355ef0ed9dc0623738059438b4a124d9290
Kryptonum <bogumil1818@gmail.com>
2023-03-09 11:19:11 +0100

    added orphans in whole website

--- src/components/sections/Layout.js
+++ src/components/sections/Layout.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GlobalStyle from "../../styles/GlobalStyle";
 import Nav from "../../components/organisms/Nav"
 
 const Layout = ({children}) => {
+  useEffect(() => {
+    const orphans = ['w','z','u','o'];
+    const paragraphs = document.querySelectorAll('p');
+    orphans.forEach(orphan => {
+      paragraphs.forEach(paragraph => {
+        console.log(orphan)
+        paragraph.innerHTML = paragraph.textContent.replace(new RegExp(` ${orphan} ` ,`g`), ` ${orphan}&nbsp;`);
+      })
+    });
+  }, [])
   return (
     <>
       <GlobalStyle />
