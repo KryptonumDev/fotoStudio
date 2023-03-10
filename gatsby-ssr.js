0ca32f1e6d2e38a630ab73f26eb0b3235507fed7
Kryptonum <bogumil1818@gmail.com>
2023-03-10 14:50:44 +0100

    added fonts preload

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,29 @@ import Layout from "./src/components/sections/Layout"
 
 export const onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: "pl" })
+  setHeadComponents([
+    <link
+      rel="preload"
+      href="/resources/fonts/DMSerifDisplay-Regular.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />,
+    <link
+      rel="preload"
+      href="/resources/fonts/Montserrat-Regular.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />,
+    <link
+      rel="preload"
+      href="/resources/fonts/Montserrat-SemiBold.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />,
+  ])
 }
 
 export const wrapPageElement = ({ props, element }) => {
